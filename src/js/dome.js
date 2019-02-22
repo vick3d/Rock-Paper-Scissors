1ac717b31c1aabf1b36fce2450873be54fad57e3
vick3d <viktoro0090@gmail.com>
2019-02-22 10:47:21 +0100

    adds computerchoice text

--- src/js/dome.js
+++ src/js/dome.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     let rps  = new Rps
     let result = document.querySelector('.result')
+    let cpuDiv = document.querySelector('.computer-choice')
 
     const rock = document.querySelector('.rock')
     const paper = document.querySelector('.paper')
@@ -8,16 +9,19 @@ document.addEventListener('DOMContentLoaded', () => {
 
     rock.addEventListener('click', function() { 
         computerChoice = rps.computerChoice()
+        cpuDiv.innerHTML = `Computer choice: ${computerChoice}` 
         result.innerHTML = rps.playGame('Rock',computerChoice)
     })
 
     paper.addEventListener('click', function() {
         computerChoice = rps.computerChoice() 
+        cpuDiv.innerHTML = `Computer choice: ${computerChoice}` 
         result.innerHTML = rps.playGame('Paper',computerChoice)
     })
     
     scissors.addEventListener('click', function() {
-        computerChoice = rps.computerChoice() 
+        computerChoice = rps.computerChoice()
+        cpuDiv.innerHTML = `Computer choice: ${computerChoice}`  
         result.innerHTML = rps.playGame('Scissors',computerChoice)
     })
 })
